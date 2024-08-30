fee03a6b38b88d8d286c5b387f8b936f6f024fed
Yannick Blanchette <yannickblanchette@hotmail.com>
2024-08-30 16:40:47 -0400

    Ajout assets background fix tailwind page

--- src/components/Abonner.jsx
+++ src/components/Abonner.jsx
@@ -11,25 +11,25 @@ const Abonner = () => {
   };
 
   return (
-    <div className="container mx-auto max-w-screen-md p-4">
-      <div className="grid grid-cols-2 gap-4 p-4 bg-gray-100 rounded-md shadow-md">
-        <div className="col-span-1 flex flex-col">
-          <h1 className="text-lg font-bold">Nom :</h1>
-          <h2 className="text-md font-semibold">Email:</h2>
-        </div>
-        <div className="col-span-1 flex flex-col items-end">
+    <div className="background-image2 min-h-screen bg-cover bg-no-repeat"> {/* Tailwind background utilities */}
+      <div className="container mx-auto max-w-screen-xl p-4">
+        <div className="max-w-lg mx-auto flex flex-col gap-6">
           {listeAbonnes.map((abonne) => (
-            <div key={abonne.email} className="flex justify-between items-center mb-2">
-              <div>
-                <p className="text-sm">{abonne.nom}</p>
-                <p className="text-sm">{abonne.email}</p>
+            <div key={abonne.email} className="p-4 bg-gray-800 rounded-md shadow-md">
+              <div className="flex justify-between items-center">
+                <div className="flex flex-col">
+                  <h1 className="text-lg font-bold text-white">Nom :</h1>
+                  <p className="text-lg font-bold text-white border-b-4 border-gray">{abonne.nom}</p>
+                  <h2 className="text-lg font-bold text-white mt-2">Email:</h2>
+                  <p className="text-lg font-bold text-white border-b-4 border-gray">{abonne.email}</p>
+                </div>
+                <button
+                  onClick={() => handleClick(abonne.email)}
+                  className="ml-4 px-4 py-4 bg-red-700 text-white rounded-md hover:bg-red-900 transition-colors duration-200"
+                >
+                  Supprimer
+                </button>
               </div>
-              <button
-                onClick={() => handleClick(abonne.email)}
-                className="ml-4 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-900 transition-colors duration-200"
-              >
-                Supprimer
-              </button>
             </div>
           ))}
         </div>
