7361d4e95f38ed990b64b70ec64de6b364ab76b7
Yannick Blanchette <yannickblanchette@hotmail.com>
2024-09-05 03:53:02 -0400

    Abonnes.jsx icon for cards added. / Add border

--- src/components/Abonner.jsx
+++ src/components/Abonner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { supprimerAbonne } from '../store/abonnesSlice';
+import { CgProfile } from "react-icons/cg";
 
 const Abonner = () => {
   const dispatch = useDispatch();
@@ -17,10 +18,15 @@ const Abonner = () => {
           <h1 className="text-3xl font-bold text-white">Liste des Abonnés</h1>
           <p className="text-lg text-gray-300">Voici la liste des personnes abonnées à votre infolettre.</p>
         </div>
-        <div className="grid grid-cols-1 gap-6">
+        <div className="grid grid-cols-1 gap-10">
           {listeAbonnes.map((abonne) => (
-            <div key={abonne.email} className="p-4 bg-gray-800 bg-opacity-90 rounded-lg shadow-lg max-w-sm mx-auto">
-              <div className="flex justify-between items-center">
+            <div key={abonne.email} className="relative p-4 bg-gray-800 bg-opacity-90 rounded-lg shadow-lg max-w-sm mx-auto border-4 border-gray-300">
+              {/* Logo at the middle top of the card */}
+              <div className="absolute -top-8 left-1/2 transform -translate-x-1/2">
+                <CgProfile className="text-white text-6xl bg-gray-800 p-2 rounded-full border-4 border-gray-300" />
+              </div>
+
+              <div className="flex justify-between items-center mt-12"> {/* Adding top margin to compensate for the icon */}
                 <div className="flex flex-col">
                   <h1 className="text-lg font-bold text-white">Nom :</h1>
                   <p className="text-md text-white border-b-2 border-gray-300">{abonne.nom}</p>
